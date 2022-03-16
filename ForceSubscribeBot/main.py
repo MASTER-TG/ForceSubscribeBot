88c43ba2caccd1f5c179641d40a87e74214b60e8
PASIYA / [ 999 ] <97598362+MASTER-TG@users.noreply.github.com>
2022-03-16 18:44:36 +0530

    Update main.py

--- ForceSubscribeBot/main.py
+++ ForceSubscribeBot/main.py
@@ -40,15 +40,12 @@ async def main(bot: Client, msg: Message):
                     await msg.chat.kick_member(user_id)
                     await msg.reply("Banned member because not joined Force Subscribe Chat")
                     return
-                buttons = [[
-                     InlineKeyboardButton(" Instagram⚡️ ", url='https://instagram.com/pasiya_lk'),
-                ],[      
-                     InlineKeyboardButton(" ⭐️Join වන්න 🔥 ", url=link)]]
+                buttons = [[InlineKeyboardButton(" ⭐️Join වන්න 🔥 ", url=link)]]
                 if action == 'mute':
                     await msg.chat.restrict_member(user_id, ChatPermissions(can_send_messages=False))
                     buttons.append([InlineKeyboardButton("Unmute Me☘️", callback_data=f"joined+{msg.from_user.id}")])
                 await msg.reply(
-                    f"⭕️ ඔයා පහල 👇 තියෙන {mention} Channel එකට Join වෙලා,👉(Unmute Me☘️)ඔබන්න🔥එතකොට ඔයාට දිගටම අපෙන් සින්දු ඉල්ලන්න පුලුවන්🤗🌺.❤️ඒවගේම අනිවාරෙන් Instagram ත් Follow කරන්න👉",
+                    f"⭕️ ඔයා පහල 👇 තියෙන {mention} Channel එකට Join වෙලා,👉(Unmute Me☘️)ඔබන්න🔥එතකොට ඔයාට දිගටම අපෙන් සින්දු ඉල්ලන්න පුලුවන්🤗🌺.❤️ඒවගේම අනිවාරෙන් Instagram ත් Follow කරන්න👉https://instagram.com/pasiya_lk⚡️",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup(buttons),
                 )
